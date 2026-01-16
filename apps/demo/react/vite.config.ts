41fd588f35f21242fdc3950ed24e69fc65f7a3f1
Seiji Kohara <sage.0118@gmail.com>
2026-01-16 19:02:40 +0900

    feat(demo): add vite aliases for development HMR (#108)

--- apps/demo/react/vite.config.ts
+++ apps/demo/react/vite.config.ts
@@ -1,9 +1,19 @@
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000,
   },
-});
+  resolve:
+    mode === "development"
+      ? {
+          alias: {
+            "@vizel/core": path.resolve(__dirname, "../../../packages/core/src"),
+            "@vizel/react": path.resolve(__dirname, "../../../packages/react/src"),
+          },
+        }
+      : undefined,
+}));
